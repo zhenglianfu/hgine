6169ce8a3738693ab92d80d565bc6f6b4bcfd48c
dingdang <dingdang@mamigumi.com>
2015-09-08 23:07:18 +0800

    sprite and core of hgine

--- source/core.js
+++ source/core.js
@@ -1,3 +1,21 @@
 /**
- * Created by Administrator on 2015/9/6.
+ * engine's core methods
  */
+
+/**
+ * constructor of Hgine
+ * @param string|HTMLCanvasElement
+ **/
+function Hgine(canvas){
+    var type = (typeof canvas);
+    if (type === 'string') {
+        return new Hgine(document.getElementById(canvas));
+    } else if (type === 'object' && canvas instanceof HTMLCanvasElement) {
+        // constructor
+
+    } else {
+        return new Error('there is no HTMLCanvasElement existed');
+    }
+}
+
+Hgine.prototype = {};
--- source/sprite.js
+++ source/sprite.js
@@ -1,3 +1,17 @@
 /**
  * Created by Administrator on 2015/9/6.
  */
+
+/** define sprite and methods **/
+function HSprite(){
+    this.x = 0;
+    this.y = 0;
+    this.width = 0;
+    this.height = 0;
+}
+
+HSprite.prototype = {
+    destroy: function(){
+
+    }
+};
